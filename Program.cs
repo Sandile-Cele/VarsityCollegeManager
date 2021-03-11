44043d7f4fcc0661293eccba0bc96537bcfa5abc
Sandile-Cele <sandilecele101@gmail.com>
2021-03-12 01:16:55 +0200

    Project files moves to root

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VarsityCollegeManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Populating data...");
+            StudentData.populateDic();
+            Console.WriteLine("Done.");
+            
+            Console.WriteLine("Printing list...");
+            
+            StudentData obj1 = new StudentData();
+
+            int i = 1;
+            foreach(FullTimeStudent stuLst in StudentData.fullTimeStudentDic){
+                Console.WriteLine("Name of no." + i + " student is: " + stuLst.Name);
+                Console.WriteLine("Age of no." + i++ + " student is: " + stuLst.Age);
+                Console.WriteLine("------------------------\n");
+            }
+        }
+    }
+}
